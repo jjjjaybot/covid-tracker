c6d43a0c3a49aa10bf3cbb00fd95489d3e062bf3
Jiazi Cai <caijiazi777@gmail.com>
2020-04-27 12:46:46 -0400

    Updates

---
+++ precache-manifest.573236676eca98ee31b23d2689ba6092.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7818311d578c4d60781b838c58f532",
+    "url": "/covid-tracker/index.html"
+  },
+  {
+    "revision": "dc9cb848e97cd9476ccb",
+    "url": "/covid-tracker/static/css/main.3e25ca40.chunk.css"
+  },
+  {
+    "revision": "57e4690195676bee8571",
+    "url": "/covid-tracker/static/js/2.4e18f4c2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-tracker/static/js/2.4e18f4c2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc9cb848e97cd9476ccb",
+    "url": "/covid-tracker/static/js/main.3c128b73.chunk.js"
+  },
+  {
+    "revision": "f210d2661cd825de531c",
+    "url": "/covid-tracker/static/js/runtime-main.b82cc920.js"
+  },
+  {
+    "revision": "d5049d728f9f831efdd5cba3a8ae71f0",
+    "url": "/covid-tracker/static/media/covid.d5049d72.jpg"
+  }
+]);
